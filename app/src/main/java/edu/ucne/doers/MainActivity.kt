74b936c26cceddbb817f37bd1522d60b5cdf5cd7
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-06 01:33:56 -0400

    Agregar @AndroidEntryPoint al MainActivity. Implementar DoersNavHost.

--- app/src/main/java/edu/ucne/doers/MainActivity.kt
+++ app/src/main/java/edu/ucne/doers/MainActivity.kt
@@ -11,15 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.doers.presentation.navigation.DoersNavHost
 import edu.ucne.doers.ui.theme.DoersTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DoersTheme {
-
+                val navHost = rememberNavController()
+                DoersNavHost(navHost)
             }
         }
     }
