eac00f400f6ab6ae9b0ba4303613a50ee0a00bd0
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-06 21:15:49 -0400

    Agrega el botón de inicio de sesión con Google a la pantalla principal.

--- app/src/main/java/edu/ucne/doers/presentation/home/HomeScreen.kt
+++ app/src/main/java/edu/ucne/doers/presentation/home/HomeScreen.kt
@@ -4,13 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.PriorityHigh
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -19,12 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import edu.ucne.doers.R
 
 @Composable
 fun HomeScreen(
+    onSignClickWithGoogle: () -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -45,6 +47,26 @@ fun HomeScreen(
                 color = MaterialTheme.colorScheme.onBackground,
                 modifier = Modifier.padding(bottom = 24.dp)
             )
+            Button(
+                onClick = onSignClickWithGoogle,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.LightGray,
+                    contentColor = Color.White
+                )
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.google_icon),
+                    contentDescription = "Google Login",
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.Unspecified
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Login With Google",
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
         }
     }
 }
