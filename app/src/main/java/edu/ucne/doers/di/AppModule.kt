222029a8b2ef2c7a3c6dfbf114a1f46b6149a076
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-12 02:28:14 -0400

    Configurar la base de datos con Room en AppModule.

--- app/src/main/java/edu/ucne/doers/di/AppModule.kt
+++ app/src/main/java/edu/ucne/doers/di/AppModule.kt
@@ -1,12 +1,16 @@
 package edu.ucne.doers.di
 
+import android.content.Context
+import androidx.room.Room
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.doers.data.local.database.DoersDb
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -21,6 +25,16 @@ object AppModule {
         Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .add(Adapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            DoersDb::class.java,
+            "Doers.db"
+        ).fallbackToDestructiveMigration()
             .build()
 
     //Firebase
