da08e8ee8d9e707727b73d80e336853ecb625ce0
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-12 01:50:14 -0400

    Definir Canjeo como una entidad con claves foráneas para Hijo y Recompensa, agregar índices, y cambiar el tipo de fechaCanjeo a Date.

--- app/src/main/java/edu/ucne/doers/data/local/entity/Canjeo.kt
+++ app/src/main/java/edu/ucne/doers/data/local/entity/Canjeo.kt
@@ -1,19 +1,22 @@
-package edu.ucne.doers.data.local.entity.relation
+package edu.ucne.doers.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import java.util.Date
 
 @Entity(
     tableName = "Canjeos",
     foreignKeys = [
         ForeignKey(entity = Hijo::class, parentColumns = ["hijoID"], childColumns = ["hijoID"], onDelete = ForeignKey.NO_ACTION),
         ForeignKey(entity = Recompensa::class, parentColumns = ["recompensaID"], childColumns = ["recompensaID"], onDelete = ForeignKey.CASCADE)
-    ]
+    ],
+    indices = [Index(value = ["hijoID", "recompensaID"])]
 )
 data class Canjeo(
     @PrimaryKey(autoGenerate = true) val canjeoID: Int = 0,
     val hijoID: Int,
     val recompensaID: Int,
-    val fechaCanjeo: String
+    val fechaCanjeo: Date
 )
