4a07fb95caf48d307dade8ebb8601cadf875d8e6
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-12 02:04:27 -0400

    Crear `enum class EstadoRecompensa`. Agregar el campo `estado` de tipo `EstadoRecompensa` a la entidad `Recompensa`. Agregar índice `salaID` a la entidad `Recompensa`.

--- app/src/main/java/edu/ucne/doers/data/local/entity/Recompensa.kt
+++ app/src/main/java/edu/ucne/doers/data/local/entity/Recompensa.kt
@@ -2,7 +2,10 @@ package edu.ucne.doers.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import edu.ucne.doers.data.local.model.EstadoRecompensa
+import java.util.Date
 
 @Entity(
     tableName = "Recompensas",
@@ -11,7 +14,8 @@ import androidx.room.PrimaryKey
         parentColumns = ["salaID"],
         childColumns = ["salaID"],
         onDelete = ForeignKey.CASCADE
-    )]
+    )],
+    indices = [Index(value = ["salaID"])]
 )
 data class Recompensa(
     @PrimaryKey(autoGenerate = true) val recompensaID: Int = 0,
@@ -19,6 +23,6 @@ data class Recompensa(
     val nombre: String,
     val imagenURL: String?,
     val puntosNecesarios: Int,
-    val tiempoLimite: String?,
-    val estado: String = "Disponible"
+    val tiempoLimite: Date?,
+    val estado: EstadoRecompensa = EstadoRecompensa.DISPONIBLE
 )
