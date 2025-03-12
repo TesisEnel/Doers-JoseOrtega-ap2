b8bf38318dd51d40d144e0e9ab3e4e9ee7770a3d
BrynOvalles <brynovallesburgos@gmail.com>
2025-03-12 19:45:30 -0400

    Creando PadreDao para interactuar con la entidad Padre en la base de datos Room.

---
+++ app/src/main/java/edu/ucne/doers/data/local/dao/PadreDao.kt
@@ -0,0 +1,33 @@
+package edu.ucne.doers.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.doers.data.local.entity.PadreEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PadreDao {
+    @Upsert
+    suspend fun save(padreEntity: PadreEntity)
+
+    @Upsert()
+    suspend fun save(padreEntity: List<PadreEntity>)
+
+    @Query(
+        """
+            SELECT *
+            FROM "Padres"
+            WHERE padreID=:id
+            LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): PadreEntity?
+
+    @Delete
+    suspend fun delete(padreEntity: PadreEntity)
+
+    @Query("SELECT * FROM Padres")
+    fun getAll(): Flow<List<PadreEntity>>
+}
