afef0d03e05de6ee282a5d52c2313661f842c72a
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-13 04:36:12 -0400

    Agregar SolicitudRecompensa a la base de datos.

--- app/src/main/java/edu/ucne/doers/data/local/database/DoersDb.kt
+++ app/src/main/java/edu/ucne/doers/data/local/database/DoersDb.kt
@@ -3,25 +3,25 @@ package edu.ucne.doers.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import edu.ucne.doers.data.local.entity.CanjeoEntity
 import edu.ucne.doers.data.local.entity.HijoEntity
-import edu.ucne.doers.data.local.entity.TransaccionHijoEntity
 import edu.ucne.doers.data.local.entity.PadreEntity
 import edu.ucne.doers.data.local.entity.RecompensaEntity
-import edu.ucne.doers.data.local.entity.SalaEntity
+import edu.ucne.doers.data.local.entity.SolicitudRecompensa
 import edu.ucne.doers.data.local.entity.TareaEntity
-import edu.ucne.doers.data.local.entity.CanjeoEntity
-import edu.ucne.doers.data.local.entity.TareaHijoEntity
+import edu.ucne.doers.data.local.entity.TareaHijo
+import edu.ucne.doers.data.local.entity.TransaccionHijo
 
 @Database(
     entities = [
         PadreEntity::class,
-        SalaEntity::class,
         HijoEntity::class,
         TareaEntity::class,
-        TareaHijoEntity::class,
+        TareaHijo::class,
         RecompensaEntity::class,
         CanjeoEntity::class,
-        TransaccionHijoEntity::class
+        TransaccionHijo::class,
+        SolicitudRecompensa::class
     ],
     version = 1
 )
