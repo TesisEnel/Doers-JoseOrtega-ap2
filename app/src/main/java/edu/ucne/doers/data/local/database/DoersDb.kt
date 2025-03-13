3631a0145dc17d4d10d2ec78f124671905e133dc
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-13 04:43:48 -0400

    Agregar todos los Dao a la base de datos.

--- app/src/main/java/edu/ucne/doers/data/local/database/DoersDb.kt
+++ app/src/main/java/edu/ucne/doers/data/local/database/DoersDb.kt
@@ -3,6 +3,14 @@ package edu.ucne.doers.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import edu.ucne.doers.data.local.dao.CanjeoDao
+import edu.ucne.doers.data.local.dao.HijoDao
+import edu.ucne.doers.data.local.dao.PadreDao
+import edu.ucne.doers.data.local.dao.RecompensaDao
+import edu.ucne.doers.data.local.dao.SolicitudRecompensaDao
+import edu.ucne.doers.data.local.dao.TareaDao
+import edu.ucne.doers.data.local.dao.TareaHijoDao
+import edu.ucne.doers.data.local.dao.TransaccionHijoDao
 import edu.ucne.doers.data.local.entity.CanjeoEntity
 import edu.ucne.doers.data.local.entity.HijoEntity
 import edu.ucne.doers.data.local.entity.PadreEntity
@@ -27,5 +35,12 @@ import edu.ucne.doers.data.local.entity.TransaccionHijo
 )
 @TypeConverters(Converters::class)
 abstract class DoersDb : RoomDatabase() {
-
+    abstract fun padreDao(): PadreDao
+    abstract fun hijoDao(): HijoDao
+    abstract fun tareaDao(): TareaDao
+    abstract fun tareaHijoDao(): TareaHijoDao
+    abstract fun recompensaDao(): RecompensaDao
+    abstract fun canjeoDao(): CanjeoDao
+    abstract fun transaccionHijoDao(): TransaccionHijoDao
+    abstract fun solicitudRecompensaDao(): SolicitudRecompensaDao
 }
