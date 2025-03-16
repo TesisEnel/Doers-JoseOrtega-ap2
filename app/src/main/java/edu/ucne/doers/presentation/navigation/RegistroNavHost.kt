77c0a7d52ac8edcfa87fc53a4655c35f4690a512
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-15 20:45:06 -0400

    Modificar para terminar signin :v

--- app/src/main/java/edu/ucne/doers/presentation/navigation/RegistroNavHost.kt
+++ app/src/main/java/edu/ucne/doers/presentation/navigation/RegistroNavHost.kt
@@ -5,11 +5,15 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import edu.ucne.doers.presentation.home.HomeScreen
+import edu.ucne.doers.presentation.padres.PadreViewModel
 import edu.ucne.doers.presentation.sign_in.GoogleAuthUiClient
 import kotlinx.coroutines.launch
 
@@ -24,6 +28,9 @@ fun DoersNavHost(
         startDestination = Screen.Home
     ) {
         composable<Screen.Home> {
+            val viewModel: PadreViewModel = hiltViewModel()
+            val state by viewModel.uiState.collectAsStateWithLifecycle()
+
             // sonar-ignore: launcher is used indirectly in onSignUsuarioClick
             val launcher = rememberLauncherForActivityResult(
                 contract = ActivityResultContracts.StartIntentSenderForResult(),
@@ -34,6 +41,10 @@ fun DoersNavHost(
                                 googleAuthUiClient.signInWithIntent(
                                     intent = result.data ?: return@launch
                                 )
+                            viewModel.onSignInResult(signInResult)
+                            navHostController.navigate(Screen.Home){
+                                popUpTo(Screen.Home) { inclusive = true }
+                            }
                         }
                     }
                 }
