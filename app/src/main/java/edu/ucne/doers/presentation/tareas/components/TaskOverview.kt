3ac01d1e767675fd36398d77b964c7abeae8e174
DjMarte <cristopher_marte@ucne.edu.do>
2025-03-17 11:59:01 -0400

    Crear `TaskOverview` para mostrar las tareas

---
+++ app/src/main/java/edu/ucne/doers/presentation/tareas/components/TaskOverview.kt
@@ -0,0 +1,81 @@
+package edu.ucne.doers.presentation.tareas.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.ucne.doers.data.local.entity.TareaEntity
+import edu.ucne.doers.data.local.model.EstadoTarea
+
+@Composable
+fun TaskOverview(
+    modifier: Modifier = Modifier,
+    tareas: List<TareaEntity>,
+    onEdit: (Int) -> Unit,
+    onDelete: (TareaEntity) -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Título principal siempre visible
+        Text(
+            text = "Reporte de Tareas",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            modifier = Modifier.padding(bottom = 16.dp, top = 22.dp)
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TaskCounter(
+                "Tareas completadas",
+                tareas.count { it.estado == EstadoTarea.COMPLETADA },
+                EstadoTarea.COMPLETADA
+            )
+            TaskCounter(
+                "Tareas pendientes",
+                tareas.count { it.estado == EstadoTarea.PENDIENTE },
+                EstadoTarea.PENDIENTE
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = if (tareas.isEmpty()) "No se han creado tareas" else "Listado de pendientes",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(tareas) { tarea ->
+                TaskCard(
+                    tarea,
+                    onEdit = onEdit,
+                    onDelete = onDelete
+                )
+            }
+        }
+    }
+}
