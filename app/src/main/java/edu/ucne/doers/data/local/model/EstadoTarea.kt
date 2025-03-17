8c2720b9c4b66c595531a1b032d4632592b97058
DjMarte <cristopher_marte@ucne.edu.do>
2025-03-17 11:41:03 -0400

    Se agregaron colores predeterminados a `EstadoTarea`.

--- app/src/main/java/edu/ucne/doers/data/local/model/EstadoTarea.kt
+++ app/src/main/java/edu/ucne/doers/data/local/model/EstadoTarea.kt
@@ -1,9 +1,11 @@
 package edu.ucne.doers.data.local.model
 
-enum class EstadoTarea(val nombreMostrable: String) {
-    PENDIENTE("Pendiente"),
-    COMPLETADA("Completada"),
-    CANCELADA("Cancelada")
+import androidx.compose.ui.graphics.Color
+
+enum class EstadoTarea(val nombreMostrable: String, val color: Color) {
+    PENDIENTE("Pendiente", Color(0xFFFFA000)),
+    COMPLETADA("Completada", Color(0xFF388E3C)),
+    CANCELADA("Cancelada", Color(0xFFD32F2F))
 }
 
 enum class EstadoTareaHijo(val nombreMostrable: String) {
