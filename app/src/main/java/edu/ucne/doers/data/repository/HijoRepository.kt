47b8db58fc67d2178c881a57f96c20576b027ac4
DjMarte <cristopher_marte@ucne.edu.do>
2025-03-26 05:50:07 -0400

    1. Se agregó TareaHijoDao a HijoRepository.
    2. Se agregando dos nuevos métodos "insertTareaHijo" y "getTareasHijo"

--- app/src/main/java/edu/ucne/doers/data/repository/HijoRepository.kt
+++ app/src/main/java/edu/ucne/doers/data/repository/HijoRepository.kt
@@ -1,12 +1,15 @@
 package edu.ucne.doers.data.repository
 
 import edu.ucne.doers.data.local.dao.HijoDao
+import edu.ucne.doers.data.local.dao.TareaHijoDao
 import edu.ucne.doers.data.local.entity.HijoEntity
+import edu.ucne.doers.data.local.entity.TareaHijo
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class HijoRepository @Inject constructor(
-    private val hijoDao: HijoDao
+    private val hijoDao: HijoDao,
+    private val tareaHijoDao: TareaHijoDao
 ) {
     suspend fun save(hijo: HijoEntity) = hijoDao.save(hijo)
 
@@ -15,4 +18,8 @@ class HijoRepository @Inject constructor(
     fun getAll(): Flow<List<HijoEntity>> = hijoDao.getAll()
 
     suspend fun delete(hijo: HijoEntity) = hijoDao.delete(hijo)
+
+    suspend fun insertTareaHijo(tareaHijo: TareaHijo) = tareaHijoDao.save(tareaHijo)
+
+    fun getTareasHijo(hijoId: Int): Flow<List<TareaHijo>> = tareaHijoDao.getByHijoId(hijoId)
 }
