70b80f8faea7ddf50a68ae37aa7f7de29ff62cc8
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-26 20:58:44 -0400

    Quitar Logs.

--- app/src/main/java/edu/ucne/doers/presentation/recompensa/padre/RecompensasListScreen.kt
+++ app/src/main/java/edu/ucne/doers/presentation/recompensa/padre/RecompensasListScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
