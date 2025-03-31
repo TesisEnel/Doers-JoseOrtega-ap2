7cc76ffb14e6aa530a93a085e8d858beb8e93594
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-03-31 14:32:50 -0400

    Actualizar HijoEntity.

--- app/src/main/java/edu/ucne/doers/data/repository/HijoRepository.kt
+++ app/src/main/java/edu/ucne/doers/data/repository/HijoRepository.kt
@@ -47,4 +47,6 @@ class HijoRepository @Inject constructor(
             Resource.Error("El código de sala no existe")
         }
     }
+
+    suspend fun updateHijo(hijo: HijoEntity) = hijoDao.update(hijo)
 }
