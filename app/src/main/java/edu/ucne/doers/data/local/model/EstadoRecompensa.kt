fb1b072aa3516c2504aee41c031309afc10b021e
Stiinger <JOSE_ORTEGA1@ucne.edu.do>
2025-04-01 13:11:37 -0400

    Marked enums as serializable.

--- app/src/main/java/edu/ucne/doers/data/local/model/EstadoRecompensa.kt
+++ app/src/main/java/edu/ucne/doers/data/local/model/EstadoRecompensa.kt
@@ -1,5 +1,8 @@
 package edu.ucne.doers.data.local.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class EstadoRecompensa(val nombreMostrable: String) {
     DISPONIBLE("Disponible"),
     CADUCADA("Caducada"),
