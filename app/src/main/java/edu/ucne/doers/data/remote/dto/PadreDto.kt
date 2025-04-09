e45d950f2f86ac6dfc23ba450344c17017f39612
Stiinger <ortegantony29@gmail.com>
2025-04-09 02:49:11 -0400

    Actualizar PadreDto para incluir el nombre de Json para la URL de la foto de perfil.

--- app/src/main/java/edu/ucne/doers/data/remote/dto/PadreDto.kt
+++ app/src/main/java/edu/ucne/doers/data/remote/dto/PadreDto.kt
@@ -1,8 +1,11 @@
 package edu.ucne.doers.data.remote.dto
 
+import com.squareup.moshi.Json
+
 data class PadreDto(
     val padreId: String,
     val nombre: String,
+    @Json(name = "fotoPerfilUrl")
     val profilePictureUrl: String?,
     val email: String,
     val codigoSala: String
