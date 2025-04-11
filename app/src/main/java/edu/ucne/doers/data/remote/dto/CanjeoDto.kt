cf3afb72ef3b52612be0e86ba06cff61096ac651
BrynOvalles <brynovallesburgos@gmail.com>
2025-04-10 21:28:27 -0400

    Changing `fecha` data type from `Date` to `String` in `CanjeoDto`

--- app/src/main/java/edu/ucne/doers/data/remote/dto/CanjeoDto.kt
+++ app/src/main/java/edu/ucne/doers/data/remote/dto/CanjeoDto.kt
@@ -1,12 +1,11 @@
 package edu.ucne.doers.data.remote.dto
 
 import edu.ucne.doers.data.local.model.EstadoRecompensa
-import java.util.Date
 
 data class CanjeoDto(
     val canjeoId: Int,
     val hijoId: Int,
     val recompensaId: Int,
-    val fecha: Date,
+    val fecha: String,
     val estado : EstadoRecompensa
 )
